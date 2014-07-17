87ecb9cc5a11b5870b9d305a7749d2d06200f03a
Ardha Herdianto <ardha2008@gmail.com>
2014-07-17 13:13:54 +0700

    Class Random

---
+++ classRandom.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+*
+*	Class Random using for unique ID
+*	Ardha @2014
+*
+*/
+
+	class Random{
+
+		public static function alpha($length = 15, $symbol = false , $case_sensitive = false ){    
+	
+		$chars = 'abcdefghijklmnopqrstuvwxyz';
+	    if ( $case_sensitive)
+	     	$chars .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';    
+		if ( $symbol )
+		    $chars .= '!@#$%^&*()';
+	        
+		$str = '';
+		for ( $i = 0; $i < $length; $i++ )
+		    $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+	        
+		return $str;
+	    }
+
+	    public static function numeric($length = 12, $symbol = false ){
+	    	$chars = '1234567890';
+	    	
+			if ( $symbol )
+			    $chars .= '!@#$%^&*()';
+		        
+			$str = '';
+			for ( $i = 0; $i < $length; $i++ )
+			    $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+		        
+			return $str;	
+	    }
+
+	    public static function all($length = 15, $symbol = true , $case_sensitive = true){
+	    	$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+	    	
+	    	if ( $case_sensitive)
+	     	$chars .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';    
+			if ( $symbol )
+		    $chars .= '!@#$%^&*()';
+	        
+			$str = '';
+			for ( $i = 0; $i < $length; $i++ )
+			    $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+		        
+			return $str;	
+	    }
+
+	}
+
+?>
